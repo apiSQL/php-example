e186a10f590576999f7f65ef4129b675fd0b6a89
tom <tom@sapletta.com>
2021-04-29 20:48:30 +0200

    example3 with config

--- index3.php
+++ index3.php
@@ -2,19 +2,26 @@
 // curl https://loadfunc.github.io/php/load_func.php --output load_func.php
 include 'load_func.php';
 
+# Load functions from remote/local
+//load_func(['https://letjson.github.io/php/let_json.php', 'https://php.apisql.com/api_sql.php'], function () {
+load_func(['https://php.letjson.com/let_json.php', 'https://php.defjson.com/def_json.php', 'https://php.apisql.com/api_sql.php'], function ($func_url_array) {
 
-# Load functions from external domains
-load_func(["https://letjson.github.io/php/let_json.php", "https://php.apisql.com/api_sql.php"], function () {
+    // Load config file from remote/local json file
+//    let_json('https://example.php.apisql.com/config.json', function($config){
+    let_json('config.json', function($config){
 
-    // Load config file
-    $config = let_json("https://php.apisql.com/config.json");
+        // load data from sqlite based on json configuration: config.json
+        api_sql($config->db, $config->sql, function ($fetch) {
 
-    // load data from sqlite based on config
-    api_sql($config->db, $config->sql, function ($fetch) {
-        var_dump($fetch['firstname']);
-        var_dump($fetch['lastname']);
-        var_dump($fetch['address']);
+            def_json('data.json', $fetch, function ($data){
+
+                let_json('data.json', function($data){
+                    var_dump($data);
+                });
+            });
+        });
     });
+
 });
 ?>
 
