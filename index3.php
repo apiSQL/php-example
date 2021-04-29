de8aae9b7b678df681d17bc53c61dd9ba6d008d4
tom <tom@sapletta.com>
2021-04-29 18:23:51 +0200

    example3 with config

--- index3.php
+++ index3.php
@@ -3,11 +3,13 @@
 include 'load_func.php';
 
 
-# Config
+# Load functions from external domains
 load_func(["https://letjson.github.io/php/let_json.php", "https://php.apisql.com/api_sql.php"], function () {
+
     // Load config file
     $config = let_json("https://php.apisql.com/config.json");
 
+    // load data from sqlite based on config
     api_sql($config->db, $config->sql, function ($fetch) {
         var_dump($fetch['firstname']);
         var_dump($fetch['lastname']);
