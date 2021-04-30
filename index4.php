d0861c0870456e0a8ce566b086f775b9be45bab9
tom <tom@sapletta.com>
2021-04-30 16:34:26 +0200

    more clear text

--- index4.php
+++ index4.php
@@ -3,9 +3,10 @@
 # curl https://loadfunc.github.io/php/load_func.php --output load_func.php
 include 'load_func.php';
 
-try {
 # Webs service with JSON
-# Load functions from remote/local
+try {
+
+    # Load functions from remote/local
     load_func(['https://php.letjson.com/let_json.php', 'https://php.defjson.com/def_json.php', 'https://php.apisql.com/api_sql.php', 'https://php.apisql.com/api_sql.php'], function ($func_url_array) {
 
         // Load config file from remote/local json file
@@ -27,7 +28,7 @@ try {
     });
 
 } catch (exception $e) {
-// Set HTTP response status code to: 500 - Internal Server Error
+    # Set HTTP response status code to: 500 - Internal Server Error
     http_response_code(500);
 }
 
